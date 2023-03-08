d5221eb46dbc972a69d2b7c4d3c1f12bcf65e962
Min97 <akar97@lab2ai.com>
2023-03-08 10:20:23 +0900

    gradle 수정

--- src/main/java/com/akar/presentation/RootController.java
+++ src/main/java/com/akar/presentation/RootController.java
@@ -1,5 +1,6 @@
 package com.akar.presentation;
 
+import com.akar.entity.UserEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,10 @@ public class RootController {
 
     @GetMapping("/test")
     public String returnTestString(){
-        return "test";
+        UserEntity userEntity = UserEntity.builder()
+                .name("test")
+                .build();
+
+        return userEntity.toString();
     }
 }
